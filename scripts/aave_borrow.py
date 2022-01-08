64f5a630b2da15c30a92a71a8bdac8fcc7c5f106
Jae Sohn <jaeyoung.sohn@gmail.com>
2022-01-08 17:55:21 +0900

    update scripts

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,6 +1,10 @@
 from brownie import config, network, interface
 from scripts.helpful_scripts import get_account
 from scripts.get_weth import get_weth
+from web3 import Web3
+
+# 0.1
+amount = Web3.toWei(0.1, "ether")
 
 def main():
     account = get_account()
@@ -12,7 +16,19 @@ def main():
     # ABI
     # Address
     lending_pool = get_lending_pool()
-    print(lending_pool)
+    
+    # approve ERC20
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
+    
+def approve_erc20(amount, spender, erc20_address, account):
+    # ABI
+    # Address
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
 
 def get_lending_pool():
     # ABI - comes from interface
