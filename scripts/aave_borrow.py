767bb200258385529fad4810b6be77e13bdda01d
Jae Sohn <jaeyoung.sohn@gmail.com>
2022-01-08 21:34:16 +0900

    update scripts

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -19,7 +19,29 @@ def main():
     
     # approve ERC20
     approve_erc20(amount, lending_pool.address, erc20_address, account)
-    
+    print("Depositing...")
+    tx = lending_pool.deposit(erc20_address, amount, account.address, 0, {"from": account})
+    tx.wait(1)
+    print("Deposited!")
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+
+def get_borrowable_data(lending_pool, account):
+    (total_collateral_eth, 
+    total_debt_eth, 
+    available_borrow_eth, 
+    current_liquidation_threshold, 
+    ltv, 
+    health_factor) = lending_pool.getUserAccountData(account.address)
+
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+
+    print(f"You have {total_collateral_eth} worth of ETH deposited.")
+    print(f"You have {total_debt_eth} worth of ETH borrowed.")
+    print(f"You can borrow {available_borrow_eth} worth of ETH.")
+    return (float(available_borrow_eth), float(total_debt_eth))
+
 def approve_erc20(amount, spender, erc20_address, account):
     # ABI
     # Address
